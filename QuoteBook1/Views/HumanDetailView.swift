80bb34ec3fda9678e5584b3c1f6364a3ceedaa27
adamTab <tabievadam@gmail.com>
2023-04-27 09:55:31 +0200

    Реализовал HumanDetailView

---
+++ QuoteBook1/Views/HumanDetailView.swift
@@ -0,0 +1,35 @@
+//
+//  HumanDetailView.swift
+//  QuoteBook1
+//
+//  Created by Адам Табиев on 27.04.2023.
+//
+
+import SwiftUI
+
+struct HumanDetailView: View {
+    var human: Human
+    
+    var body: some View {
+        ScrollView {
+            Image(human.imageName)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(.orange, lineWidth: 4))
+                .shadow(radius: 10)
+                .padding()
+            Text(human.name)
+                .font(.title)
+            Text(human.briefBio)
+                .font(.subheadline)
+            Divider()
+            Text(human.biography ?? "")
+                .font(.headline)
+                .multilineTextAlignment(.center)
+        }.padding(EdgeInsets(top: 30, leading: 20, bottom: 20, trailing: 20))    }
+}
+
+struct HumanDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        HumanDetailView(human: .sampleForBio)
+    }
+}
